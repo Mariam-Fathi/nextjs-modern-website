3257fdeb0a409a1ed108d19292ad967b90d18d00
Mariam-Fathi <66276072+Mariam-Fathi@users.noreply.github.com>
2023-08-03 17:31:01 +0200

    deploy

--- constants/index.js
+++ constants/index.js
@@ -26,12 +26,6 @@ export const servicesData = [
   },
 ];
 
-export const startingFeatures = [
-  'Find a world that suits you and you want to enter',
-  'Enter the world by reading basmalah to be safe',
-  'No need to beat around the bush, just stay on the gas and have fun',
-];
-
 export const workData = [
   {
     imgUrl: '/seo-optimization.png',
